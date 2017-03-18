774d8940047911b758c64e4c81d4121e93dd53a2
Andrei Apostol <aa16366@it052568.users.bris.ac.uk>
2017-03-18 17:34:02 +0000

    Small difference

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
@@ -1,23 +1,26 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.gamekit.graph.Graph;
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
-import uk.ac.bris.cs.scotlandyard.model.Colour;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.Player;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 import java.util.Set;
 import java.util.function.Consumer;
 
 // TODO name the AI
 @ManagedAI("Elodia")
+
 public class MyAI implements PlayerFactory {
+
 
 	// TODO create a new player here
 	@Override
+	public Graph<Integer, Transport> graph;
 	public Player createPlayer(Colour colour) {
 		return new MyPlayer();
 	}
@@ -25,6 +28,10 @@ public class MyAI implements PlayerFactory {
 	// TODO A sample player that selects a random move
 	private static class MyPlayer implements Player {
 
+
+		private final int DijkstraDistance (ScotlandYardView view, int source, int destination){
+			Collection<Edge<Integer, Transport>> edges = view.getGraph().getEdgesFrom(view.getGraph().getNode(source));
+		}
 		private final Random random = new Random();
 
 		@Override
